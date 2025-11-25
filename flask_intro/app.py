6ab47805f95c4f4b7b735b706d15b484fd869f35
marsa <marsadc07@gmail.com>
2025-11-25 16:24:20 +0000

    Day2: Connected the Add task button to a new route that adds a new task + added a feature that allows users to remove tasks through checkboxes

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
 
 tasks = [
     {"number": 1, "name": "Buy Milk", "description": "go to the shop", "date": "23/11/25"},
@@ -11,6 +12,30 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route("/")
 def home():
     return render_template("tasks.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    name = request.form["Task Name"]
+    description = request.form["Task Description"]
+    button = request.form["Add Task"]
+    tasks.append({
+        "number": len(tasks) + 1,
+        "name": name,
+        "description": description,
+        "date": datetime.now().strftime("%m/%d/%Y")
+
+    })
+    return redirect(url_for("home"))
+
+@app.route("/delete", methods=["POST"])
+def delete_task():
+    task_number = int(request.form["Action"])
+    for task in tasks:
+        if task["number"] == task_number:
+            tasks.remove(task)
+            break
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
