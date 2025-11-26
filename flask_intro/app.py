3403c760a506a075a5fe9241e338beca321a2557
marsa <marsadc07@gmail.com>
2025-11-26 12:25:21 +0000

    Day3: Created a new table Completed tasks and added a feature that removes tasks + moves task into completed table

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -2,42 +2,41 @@ from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 
 tasks = [
-    {"number": 1, "name": "Buy Milk", "description": "go to the shop", "date": "23/11/25"},
-    {"number": 2, "name": "Homework", "description": "finish maths homework", "date": "23/11/25"},
-    {"number": 3, "name": "Gym", "description": "train back and arms", "date": "24/11/25"},
+    {"number": 1, "name": "Buy Milk", "description": "go to the shop", "date": "23/11/25", "completed": False},
+    {"number": 2, "name": "Homework", "description": "finish maths homework", "date": "23/11/25", "completed": False},
+    {"number": 3, "name": "Gym", "description": "train back and arms", "date": "24/11/25", "completed": False},
 ]
+completed_tasks = []
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 def home():
-    return render_template("tasks.html", tasks=tasks)
+    return render_template("tasks.html", tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route("/add", methods=["POST"])
 def add_task():
     name = request.form["Task Name"]
     description = request.form["Task Description"]
-    button = request.form["Add Task"]
     tasks.append({
         "number": len(tasks) + 1,
         "name": name,
         "description": description,
-        "date": datetime.now().strftime("%m/%d/%Y")
-
+        "date": datetime.now().strftime("%m/%d/%Y"),
+        "completed": False
     })
     return redirect(url_for("home"))
 
-@app.route("/delete", methods=["POST"])
-def delete_task():
-    task_number = int(request.form["Action"])
+@app.route("/complete", methods=["POST"])
+def complete_task():
+    task_number = int(request.form["task_number"])
     for task in tasks:
         if task["number"] == task_number:
+            task["completed"] = True
+            completed_tasks.append(task)
             tasks.remove(task)
             break
     return redirect(url_for("home"))
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
