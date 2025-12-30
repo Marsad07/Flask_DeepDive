f54091dfa8cf3fd87ffc6405f9f8e57cdc80caca
marsa <marsadc07@gmail.com>
2025-12-30 19:36:45 +0000

    Day9: created due_date section

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -14,10 +14,10 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/')
 def homepage():
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT COUNT(*) AS active_count FROM tasks WHERE status = 'active'")
+    cursor.execute("SELECT COUNT(*) AS active_count FROM tasks_new WHERE status = 'active'")
     active_count = cursor.fetchone()['active_count']
 
-    cursor.execute("SELECT COUNT(*) AS completed_count FROM tasks WHERE status = 'completed'")
+    cursor.execute("SELECT COUNT(*) AS completed_count FROM tasks_new WHERE status = 'completed'")
     completed_count = cursor.fetchone()['completed_count']
 
     return render_template('homepage.html', active_count=active_count, completed_count=completed_count)
@@ -25,10 +25,10 @@ def homepage():
 @app.route("/tasks")
 def tasks_page():
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM tasks WHERE status='active'")
+    cursor.execute("SELECT * FROM tasks_new WHERE status='active' ORDER BY due_date IS NULL, due_date ASC")
     tasks = cursor.fetchall()
 
-    cursor.execute("SELECT * FROM tasks WHERE status = 'completed'")
+    cursor.execute("SELECT * FROM tasks_new WHERE status = 'completed'")
     completed_tasks = cursor.fetchall()
     return render_template("tasks.html", tasks=tasks, completed_tasks=completed_tasks)
 
@@ -41,12 +41,13 @@ def add_task():
     name = request.form["Task Name"]
     description = request.form["Task Description"]
     priority = request.form.get("priority")
+    due_date = request.form["due_date"]
 
     cursor = db.cursor()
     cursor.execute(
-        "INSERT INTO tasks (task_name, task_description, priority, status, added_date) "
-        "VALUES (%s, %s, %s, 'active', NOW())",
-        (name, description, priority)
+        "INSERT INTO tasks_new (task_name, task_description, priority, status, added_date, due_date) "
+        "VALUES (%s, %s, %s, 'active', NOW(), %s)",
+        (name, description, priority, due_date)
     )
     db.commit()
     return redirect(url_for("add_task_page"))
@@ -56,7 +57,7 @@ def complete_task():
     task_id = int(request.form["task_number"])
     cursor = db.cursor()
     cursor.execute(
-        "UPDATE tasks SET status='completed', completed_date=NOW() WHERE id=%s",
+        "UPDATE tasks_new SET status='completed', completed_date=NOW() WHERE id=%s",
         (task_id,)
     )
     db.commit()
@@ -66,7 +67,7 @@ def complete_task():
 @app.route("/clear_completed", methods=["POST"])
 def clear_completed():
     cursor = db.cursor()
-    cursor.execute("DELETE FROM tasks WHERE status='completed'")
+    cursor.execute("DELETE FROM tasks_new WHERE status='completed'")
     db.commit()
     return redirect(url_for("tasks_page"))
 
@@ -74,7 +75,7 @@ def clear_completed():
 @app.route('/testdb')
 def testdb():
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM tasks")
+    cursor.execute("SELECT * FROM tasks_new")
     tasks = cursor.fetchall()
     return str(tasks)
 
@@ -84,12 +85,12 @@ def edit_task(task_id):
     if request.method == "POST":
         new_task_name = request.form["task_name"]
         new_task_description = request.form["task_description"]
-        cursor.execute("UPDATE tasks SET task_name=%s, task_description=%s WHERE id=%s",
+        cursor.execute("UPDATE tasks_new SET task_name=%s, task_description=%s WHERE id=%s",
                        (new_task_name, new_task_description, task_id))
         db.commit()
         return redirect(url_for("tasks_page"))
     else:
-        cursor.execute("SELECT * FROM tasks WHERE id=%s", (task_id,))
+        cursor.execute("SELECT * FROM tasks_new WHERE id=%s", (task_id,))
         task = cursor.fetchone()
         return render_template("edit.html", task=task)
 
