7b7fd6b06e764668cacd4342c200a0511adabca4
marsa <marsadc07@gmail.com>
2026-03-07 11:14:16 +0000

    Added a footer to the bottom of each page for restaurant website

--- flask_intro/app2/views/general.py
+++ flask_intro/app2/views/general.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from app2.controllers.general_controller import (
-    home_page, about_page, contact_page
+    home_page, about_page, contact_page, newsletter_signup
 )
 
 general_bp = Blueprint("general", __name__)
@@ -8,3 +8,4 @@ general_bp = Blueprint("general", __name__)
 general_bp.route("/")(home_page)
 general_bp.route("/about")(about_page)
 general_bp.route("/contact")(contact_page)
+general_bp.route("/newsletter", methods=["POST"])(newsletter_signup)
