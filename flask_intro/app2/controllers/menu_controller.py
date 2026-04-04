037c01f366352095a9963f5786723f779e8d8931
marsa <marsadc07@gmail.com>
2026-04-04 22:09:47 +0100

    Created a smooth switch for customers / staff members to switch between login routes

--- flask_intro/app2/controllers/menu_controller.py
+++ flask_intro/app2/controllers/menu_controller.py
@@ -3,7 +3,8 @@ from app2.database import get_db
 
 
 def show_menu():
-    cursor = get_db().cursor(dictionary=True)
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
     cursor.execute("SELECT * FROM menu_items WHERE is_available = TRUE ORDER BY category, item_name")
     menu_items = cursor.fetchall()
-    return render_template("menu.html", menu_items=menu_items)
+    return render_template('menu.html', menu_items=menu_items)
